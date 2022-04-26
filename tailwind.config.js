a60611ec9fe75b2d4b39ce10e191e795148d2e05
Alex Crocker <croc@hey.com>
2022-04-26 15:58:19 -0400

    disables dark mode temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  darkMode: "media",
+  darkMode: false,
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
